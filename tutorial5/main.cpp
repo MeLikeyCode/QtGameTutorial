85209acd133edc7629edb6968764c2d0e9446b12
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2014-11-17 23:01:18 -0800

    Create main.cpp

---
+++ tutorial5/main.cpp
@@ -0,0 +1,48 @@
+#include <QApplication>
+#include "MyRect.h"
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QTimer>
+
+/*
+Tutorial Topics:
+-collidingItems() <- new topic
+-QTimer and coordinate systems <- old topics (review)
+*/
+
+int main(int argc, char *argv[]){
+    QApplication a(argc, argv);
+
+    // create a scene
+    QGraphicsScene * scene = new QGraphicsScene();
+
+    // create an item to add to the scene
+    MyRect * player = new MyRect();
+    player->setRect(0,0,100,100); // change the rect from 0x0 (default) to 100x100 pixels
+
+    // add the item to the scene
+    scene->addItem(player);
+
+    // make rect focusable
+    player->setFlag(QGraphicsItem::ItemIsFocusable);
+    player->setFocus();
+
+    // create a view to visualize the scene
+    QGraphicsView * view = new QGraphicsView(scene);
+    view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+    // show the view
+    view->show();
+    view->setFixedSize(800,600);
+    scene->setSceneRect(0,0,800,600);
+
+    player->setPos(view->width()/2,view->height() - player->rect().height());
+
+    // spawn enemies
+    QTimer * timer = new QTimer();
+    QObject::connect(timer,SIGNAL(timeout()),player,SLOT(spawn()));
+    timer->start(2000);
+
+    return a.exec();
+}
