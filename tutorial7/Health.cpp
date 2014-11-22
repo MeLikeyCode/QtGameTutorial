6a90d90691aa87bc445c566e626520d17dc0c1ed
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2014-11-22 13:50:38 -0800

    Create Health.cpp

---
+++ tutorial7/Health.cpp
@@ -0,0 +1,22 @@
+#include "Health.h"
+#include <QFont>
+
+Health::Health(QGraphicsItem *parent): QGraphicsTextItem(parent){
+    // initialize the score to 0
+    health = 3;
+
+    // draw the text
+    setPlainText(QString("Health: ") + QString::number(health)); // Health: 3
+    setDefaultTextColor(Qt::red);
+    setFont(QFont("times",16));
+}
+
+void Health::decrease(){
+    health--;
+    setPlainText(QString("Health: ") + QString::number(health)); // Health: 2
+}
+
+int Health::getHealth(){
+    return health;
+}
+
