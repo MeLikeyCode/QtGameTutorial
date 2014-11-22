abd81e93d3b4b60e8881d16c324e0f09ca005c1f
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2014-11-22 13:50:26 -0800

    Create Game.cpp

---
+++ tutorial7/Game.cpp
@@ -0,0 +1,48 @@
+#include "Game.h"
+#include <QTimer>
+#include <QGraphicsTextItem>
+#include <QFont>
+#include "Enemy.h"
+#include <QMediaPlayer>
+
+Game::Game(QWidget *parent){
+    // create the scene
+    scene = new QGraphicsScene();
+    scene->setSceneRect(0,0,800,600); // make the scene 800x600 instead of infinity by infinity (default)
+
+    // make the newly created scene the scene to visualize (since Game is a QGraphicsView Widget,
+    // it can be used to visualize scenes)
+    setScene(scene);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setFixedSize(800,600);
+
+    // create the player
+    player = new Player();
+    player->setRect(0,0,100,100); // change the rect from 0x0 (default) to 100x100 pixels
+    player->setPos(400,500); // TODO generalize to always be in the middle bottom of screen
+    // make the player focusable and set it to be the current focus
+    player->setFlag(QGraphicsItem::ItemIsFocusable);
+    player->setFocus();
+    // add the player to the scene
+    scene->addItem(player);
+
+    // create the score/health
+    score = new Score();
+    scene->addItem(score);
+    health = new Health();
+    health->setPos(health->x(),health->y()+25);
+    scene->addItem(health);
+
+    // spawn enemies
+    QTimer * timer = new QTimer();
+    QObject::connect(timer,SIGNAL(timeout()),player,SLOT(spawn()));
+    timer->start(2000);
+
+    // play background music
+    QMediaPlayer * music = new QMediaPlayer();
+    music->setMedia(QUrl("qrc:/sounds/bgsound.mp3"));
+    music->play();
+
+    show();
+}
