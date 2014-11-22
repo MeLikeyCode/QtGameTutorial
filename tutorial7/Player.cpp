fd621b0ffca6d2e1c316659c7e163ba925e82375
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2014-11-22 13:51:03 -0800

    Create Player.cpp

---
+++ tutorial7/Player.cpp
@@ -0,0 +1,44 @@
+#include "Player.h"
+#include <QGraphicsScene>
+#include <QKeyEvent>
+#include "Bullet.h"
+#include "Enemy.h"
+
+Player::Player(QGraphicsItem *parent): QGraphicsRectItem(parent){
+    bulletsound = new QMediaPlayer();
+    bulletsound->setMedia(QUrl("qrc:/sounds/bullet.wav"));
+}
+
+void Player::keyPressEvent(QKeyEvent *event){
+    // move the player left and right
+    if (event->key() == Qt::Key_Left){
+        if (pos().x() > 0)
+        setPos(x()-10,y());
+    }
+    else if (event->key() == Qt::Key_Right){
+        if (pos().x() + 100 < 800)
+        setPos(x()+10,y());
+    }
+    // shoot with the spacebar
+    else if (event->key() == Qt::Key_Space){
+        // create a bullet
+        Bullet * bullet = new Bullet();
+        bullet->setPos(x(),y());
+        scene()->addItem(bullet);
+
+        // play bulletsound
+        if (bulletsound->state() == QMediaPlayer::PlayingState){
+            bulletsound->setPosition(0);
+        }
+        else if (bulletsound->state() == QMediaPlayer::StoppedState){
+            bulletsound->play();
+        }
+
+    }
+}
+
+void Player::spawn(){
+    // create an enemy
+    Enemy * enemy = new Enemy();
+    scene()->addItem(enemy);
+}
