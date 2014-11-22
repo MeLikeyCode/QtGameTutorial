f4efc58669ea2be2c2da79c71f2f79412230da87
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2014-11-21 22:36:52 -0800

    Create Bullet.cpp

---
+++ tutorial6/Bullet.cpp
@@ -0,0 +1,52 @@
+#include "Bullet.h"
+#include <QTimer>
+#include <QGraphicsScene>
+#include <QList>
+#include "Enemy.h"
+#include "Game.h"
+
+extern Game * game; // there is an external global object called game
+
+Bullet::Bullet(QGraphicsItem *parent): QObject(), QGraphicsRectItem(parent){
+    // drew the bullet (a rectangle)
+    setRect(0,0,10,50);
+
+    // make/connect a timer to move() the bullet every so often
+    QTimer * timer = new QTimer(this);
+    connect(timer,SIGNAL(timeout()),this,SLOT(move()));
+
+    // start the timer
+    timer->start(50);
+}
+
+void Bullet::move(){
+    // get a list of all the items currently colliding with this bullet
+    QList<QGraphicsItem *> colliding_items = collidingItems();
+
+    // if one of the colliding items is an Enemy, destroy both the bullet and the enemy
+    for (int i = 0, n = colliding_items.size(); i < n; ++i){
+        if (typeid(*(colliding_items[i])) == typeid(Enemy)){
+            // increase the score
+            game->score->increase();
+
+            // remove them from the scene (still on the heap)
+            scene()->removeItem(colliding_items[i]);
+            scene()->removeItem(this);
+
+            // delete them from the heap to save memory
+            delete colliding_items[i];
+            delete this;
+
+            // return (all code below refers to a non existint bullet)
+            return;
+        }
+    }
+
+    // if there was no collision with an Enemy, move the bullet forward
+    setPos(x(),y()-10);
+    // if the bullet is off the screen, destroy it
+    if (pos().y() + rect().height() < 0){
+        scene()->removeItem(this);
+        delete this;
+    }
+}
