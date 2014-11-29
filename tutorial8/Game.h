776330e52d2385e24e1ee57a6bfc69904479bb4f
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2014-11-29 10:57:46 -0800

    Create Game.h

---
+++ tutorial8/Game.h
@@ -0,0 +1,22 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <QGraphicsView>
+#include <QWidget>
+#include <QGraphicsScene>
+#include "Player.h"
+#include "Score.h"
+#include "Health.h"
+
+class Game: public QGraphicsView{
+public:
+    Game(QWidget * parent=0);
+
+    QGraphicsScene * scene;
+    Player * player;
+    Score * score;
+    Health * health;
+
+};
+
+#endif // GAME_H
