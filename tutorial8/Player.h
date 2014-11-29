b3e02f8441f90d422c32c0d32873193df2d5934f
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2014-11-29 10:58:25 -0800

    Create Player.h

---
+++ tutorial8/Player.h
@@ -0,0 +1,20 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <QGraphicsPixmapItem>
+#include <QObject>
+#include <QGraphicsItem>
+#include <QMediaPlayer>
+
+class Player:public QObject, public QGraphicsPixmapItem{
+    Q_OBJECT
+public:
+    Player(QGraphicsItem * parent=0);
+    void keyPressEvent(QKeyEvent * event);
+public slots:
+    void spawn();
+private:
+    QMediaPlayer * bulletsound;
+};
+
+#endif // PLAYER_H
