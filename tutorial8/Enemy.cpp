b907873d8aff74690e28f718255aa36a6b1ad5d9
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2014-11-29 10:59:21 -0800

    Create Enemy.cpp

---
+++ tutorial8/Enemy.cpp
@@ -0,0 +1,40 @@
+#include "Enemy.h"
+#include <QTimer>
+#include <QGraphicsScene>
+#include <QList>
+#include <stdlib.h> // rand() -> really large int
+#include "Game.h"
+
+extern Game * game;
+
+Enemy::Enemy(QGraphicsItem *parent): QObject(), QGraphicsPixmapItem(parent){
+    //set random x position
+    int random_number = rand() % 700;
+    setPos(random_number,0);
+
+    // drew the rect
+    setPixmap(QPixmap(":/images/enemy.png"));
+    setTransformOriginPoint(50,50);
+    setRotation(180);
+
+    // make/connect a timer to move() the enemy every so often
+    QTimer * timer = new QTimer(this);
+    connect(timer,SIGNAL(timeout()),this,SLOT(move()));
+
+    // start the timer
+    timer->start(50);
+}
+
+void Enemy::move(){
+    // move enemy down
+    setPos(x(),y()+5);
+
+    // destroy enemy when it goes out of the screen
+    if (pos().y() > 600){
+        //decrease the health
+        game->health->decrease();
+
+        scene()->removeItem(this);
+        delete this;
+    }
+}
