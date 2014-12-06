3dacd8e78fac16b4cf470db235e745740182c6b5
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2014-12-06 10:06:20 -0800

    Create Game.cpp

---
+++ Tutorial10/Game.cpp
@@ -0,0 +1,34 @@
+#include "Game.h"
+#include <QGraphicsScene>
+#include "Tower.h"
+#include "Bullet.h"
+
+Game::Game(): QGraphicsView(){
+    // create a scene
+    scene = new QGraphicsScene(this);
+    scene->setSceneRect(0,0,800,600);
+
+    // set the scene
+    setScene(scene);
+
+    // create a tower
+    Tower * t = new Tower();
+    t->setPos(100,100);
+
+    // add the tower to scene
+    scene->addItem(t);
+
+    setFixedSize(800,600);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+}
+
+void Game::mousePressEvent(QMouseEvent *event){
+    //create a bullet
+    Bullet * bullet = new Bullet();
+    bullet->setPos(event->pos());
+    bullet->setRotation(40);
+    scene->addItem(bullet);
+
+}
