09a119a4974b8449ade53130a962569bc447fe88
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2014-12-06 10:05:24 -0800

    Create Bullet.h

---
+++ Tutorial10/Bullet.h
@@ -0,0 +1,16 @@
+#ifndef BULLET_H
+#define BULLET_H
+
+#include <QGraphicsPixmapItem>
+#include <QObject>
+#include <QGraphicsItem>
+
+class Bullet: public QObject, public QGraphicsPixmapItem{
+    Q_OBJECT
+public:
+    Bullet(QGraphicsItem * parent=0);
+public slots:
+    void move();
+};
+
+#endif // BULLET_H
