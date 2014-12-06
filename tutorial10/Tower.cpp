917dae5ded9d93f0e0e7f3373d8505c2bea78993
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2014-12-06 00:14:47 -0800

    Create Tower.cpp

---
+++ tutorial10/Tower.cpp
@@ -0,0 +1,37 @@
+#include "Tower.h"
+#include <QPixmap>
+#include <QVector>
+#include <QPointF>
+#include <QPolygonF>
+
+// list of points: (1,0), (2,0), (3,1), (3,2), (2,3), (1,3), (0,2), (0,1)
+
+
+Tower::Tower(QGraphicsItem *parent){
+    // set the graphics
+    setPixmap(QPixmap(":/images/tower.png"));
+
+    // create points vector
+    QVector<QPointF> points;
+    points << QPoint(1,0) << QPoint(2,0) << QPoint(3,1) << QPoint(3,2) << QPoint(2,3)
+               << QPoint(1,3) << QPoint(0,2) << QPoint(0,1);
+
+    // scale points
+    int SCALE_FACTOR = 75;
+    for (size_t i = 0, n = points.size(); i < n; i++){
+        points[i] *= SCALE_FACTOR;
+    }
+
+    // create the QGraphicsPolygonItem
+    attack_area = new QGraphicsPolygonItem(QPolygonF(points),this);
+
+    // move the polygon
+    QPointF poly_center(1.5,1.5);
+    poly_center *= SCALE_FACTOR;
+    poly_center = mapToScene(poly_center);;
+    QPointF tower_center(x()+44,y()+44);
+    QLineF ln(poly_center,tower_center);
+    attack_area->setPos(x()+ln.dx(),y()+ln.dy());
+
+
+}
