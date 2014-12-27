793c9fcce6be6826f5f182d4a09497a612402807
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2014-12-26 17:20:33 -0800

    Create Bullet.cpp

---
+++ tutorial14/Bullet.cpp
@@ -0,0 +1,32 @@
+#include "Bullet.h"
+#include <QPixmap>
+#include <QTimer>
+#include <qmath.h> // qSin, qCos, qTan
+#include "Game.h"
+
+extern Game * game;
+
+Bullet::Bullet(QGraphicsItem *parent): QObject(),QGraphicsPixmapItem(parent){
+    // set graphics
+    setPixmap(QPixmap(":images/arrow.png"));
+
+
+    // connect a timer to move()
+    QTimer * move_timer = new QTimer(this);
+    connect(move_timer,SIGNAL(timeout()),this,SLOT(move()));
+    move_timer->start(50);
+}
+
+
+
+
+void Bullet::move(){
+    int STEP_SIZE = 30;
+    double theta = rotation(); // degrees
+
+    double dy = STEP_SIZE * qSin(qDegreesToRadians(theta));
+    double dx = STEP_SIZE * qCos(qDegreesToRadians(theta));
+
+    setPos(x()+dx, y()+dy);
+
+}
