b76332850a9b43885199575d990db46027277ad2
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2015-01-05 01:05:53 -0800

    Create Game.h

---
+++ tutorial15/Game.h
@@ -0,0 +1,24 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <QGraphicsView>
+#include <QMouseEvent>
+#include <QGraphicsPixmapItem>
+#include "Tower.h"
+
+
+class Game: public QGraphicsView{
+public:
+    // member functions
+    Game();
+    void setCursor(QString filename);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+
+    // member attributes
+    QGraphicsScene * scene;
+    QGraphicsPixmapItem * cursor;
+    Tower * building;
+};
+
+#endif // GAME_H
