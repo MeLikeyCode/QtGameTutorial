7def44c6ff7a9363483721c378efe41022b1e913
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2015-01-18 01:04:31 -0800

    Create Tower.h

---
+++ tutorial16/Tower.h
@@ -0,0 +1,24 @@
+#ifndef TOWER_H
+#define TOWER_H
+
+#include <QGraphicsPixmapItem>
+#include <QGraphicsPolygonItem>
+#include <QGraphicsItem>
+#include <QPointF>
+#include <QObject>
+
+class Tower:public QObject, public QGraphicsPixmapItem{
+    Q_OBJECT
+public:
+    Tower(QGraphicsItem * parent=0);
+    double distanceTo(QGraphicsItem * item);
+    virtual void fire();
+public slots:
+    void aquire_target();
+protected:
+    QGraphicsPolygonItem * attack_area;
+    QPointF attack_dest;
+    bool has_target;
+};
+
+#endif // TOWER_H
