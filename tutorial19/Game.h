0521ef88208c21c98539b05f0163b027db1a6fb9
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2015-02-22 12:53:18 -0800

    Create Game.h

---
+++ tutorial19/Game.h
@@ -0,0 +1,22 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <QGraphicsView>
+#include <QGraphicsScene>
+// #include "HexBoard.h"
+
+class Game: public QGraphicsView{
+public:
+    // constructors
+    Game(QWidget* parent=NULL);
+
+    // public methods
+    void start();
+
+    // public attributes
+    QGraphicsScene* scene;
+    // HexBoard* hexBoard; TODO
+    QString whosTurn;
+};
+
+#endif // GAME_H
