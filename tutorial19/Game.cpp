5723813b85efe8db7604797392c0dd134500602f
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2015-02-22 12:54:18 -0800

    Update and rename tutorial19 to tutorial19/Game.cpp

---
+++ tutorial19/Game.cpp
@@ -0,0 +1,18 @@
+#include "Game.h"
+
+Game::Game(QWidget *parent){
+    // set up the screen
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setFixedSize(1024,768);
+
+    // set up the scene
+    scene = new QGraphicsScene();
+    scene->setSceneRect(0,0,1024,768);
+    setScene(scene);
+}
+
+void Game::start(){
+    // test code TODO remove
+
+}
