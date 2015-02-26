d2f868190db05c9dba3ce7913c58989b9ff75f3a
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2015-02-26 01:21:59 -0800

    Update and rename tutorial23 to tutorial23/Game.cpp

---
+++ tutorial23/Game.cpp
@@ -0,0 +1,52 @@
+#include "Game.h"
+#include "HexBoard.h"
+#include "Button.h"
+#include <QGraphicsTextItem>
+
+Game::Game(QWidget *parent){
+    // set up the screen
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setFixedSize(1024,768);
+
+    // set up the scene
+    scene = new QGraphicsScene();
+    scene->setSceneRect(0,0,1024,768);
+    setScene(scene);
+}
+
+void Game::start(){
+    // clear the screen
+    scene->clear();
+
+    // test code TODO remove
+    hexBoard = new HexBoard();
+    hexBoard->placeHexes(100,100,3,3);
+}
+
+void Game::displayMainMenu(){
+    // create the title text
+    QGraphicsTextItem* titleText = new QGraphicsTextItem(QString("Hex Warz"));
+    QFont titleFont("comic sans",50);
+    titleText->setFont(titleFont);
+    int txPos = this->width()/2 - titleText->boundingRect().width()/2;
+    int tyPos = 150;
+    titleText->setPos(txPos,tyPos);
+    scene->addItem(titleText);
+
+    // create the play button
+    Button* playButton = new Button(QString("Play"));
+    int bxPos = this->width()/2 - playButton->boundingRect().width()/2;
+    int byPos = 275;
+    playButton->setPos(bxPos,byPos);
+    connect(playButton,SIGNAL(clicked()),this,SLOT(start()));
+    scene->addItem(playButton);
+
+    // create the quit button
+    Button* quitButton = new Button(QString("Quit"));
+    int qxPos = this->width()/2 - quitButton->boundingRect().width()/2;
+    int qyPos = 350;
+    quitButton->setPos(qxPos,qyPos);
+    connect(quitButton,SIGNAL(clicked()),this,SLOT(close()));
+    scene->addItem(quitButton);
+}
