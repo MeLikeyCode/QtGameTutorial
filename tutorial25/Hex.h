80809d8992e47856cd375366bcb888c20c4aef78
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2015-02-27 13:31:35 -0800

    Update and rename tutorial25 to tutorial25/Hex.h

---
+++ tutorial25/Hex.h
@@ -0,0 +1,27 @@
+#ifndef HEX_H
+#define HEX_H
+
+#include <QGraphicsPolygonItem>
+
+class Hex: public QGraphicsPolygonItem{
+public:
+    // constructors
+    Hex(QGraphicsItem* parent=NULL);
+    // getters
+    int getAttackOf(int side);
+
+    // setters
+    void setAttackOf(int side, int attack);
+    void setOwner(QString player);
+
+private:
+    QString owner;
+    int side0Attack;
+    int side1Attack;
+    int side2Attack;
+    int side3Attack;
+    int side4Attack;
+    int side5Attack;
+};
+
+#endif // HEX_H
