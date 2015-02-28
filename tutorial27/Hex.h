a3a92bdbb2888c85ed6253b64fb073ef5a38950b
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2015-02-27 17:45:28 -0800

    Update and rename tutorial27 to tutorial27/Hex.h

---
+++ tutorial27/Hex.h
@@ -0,0 +1,35 @@
+#ifndef HEX_H
+#define HEX_H
+
+#include <QGraphicsPolygonItem>
+#include <QGraphicsSceneMouseEvent>
+
+class Hex: public QGraphicsPolygonItem{
+public:
+    // constructors
+    Hex(QGraphicsItem* parent=NULL);
+    // getters
+    int getAttackOf(int side);
+    bool getIsPlaced();
+    QString getOwner();
+
+    // event
+    void mousePressEvent(QGraphicsSceneMouseEvent* event);
+
+    // setters
+    void setAttackOf(int side, int attack);
+    void setOwner(QString player);
+    void setIsPlaced(bool b);
+
+private:
+    bool isPlaced;
+    QString owner;
+    int side0Attack;
+    int side1Attack;
+    int side2Attack;
+    int side3Attack;
+    int side4Attack;
+    int side5Attack;
+};
+
+#endif // HEX_H
