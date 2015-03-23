fc2b2050314280255471df0403d2c37d60191c07
Abdullah Aghazadah <abdullah.aghazadah@gmail.com>
2015-03-23 00:11:32 -0700

    Create Hex.h

---
+++ tutorial34/Hex.h
@@ -0,0 +1,44 @@
+#ifndef HEX_H
+#define HEX_H
+
+#include <QGraphicsPolygonItem>
+#include <QGraphicsSceneMouseEvent>
+
+class Hex: public QGraphicsPolygonItem{
+public:
+    // constructors
+    Hex(QGraphicsItem* parent=NULL);
+    // getters
+    int getAttackOf(int side);
+    bool getIsPlaced();
+    QString getOwner();
+
+    // event
+    void mousePressEvent(QGraphicsSceneMouseEvent* event);
+
+    // setters
+    void setAttackOf(int side, int attack);
+    void setOwner(QString player);
+    void setIsPlaced(bool b);
+
+    void displaySideAttacks();
+    void createLines();
+    void findNeighbors();
+    void switchOwner();
+    void captureNeighbors();
+
+private:
+    QList<QGraphicsLineItem*> lines;
+    QList<Hex*> neighbors;
+    bool isPlaced;
+    QString owner;
+    int side0Attack;
+    int side1Attack;
+    int side2Attack;
+    int side3Attack;
+    int side4Attack;
+    int side5Attack;
+    QList<QGraphicsTextItem*> attackTexts;
+};
+
+#endif // HEX_H
